10844b9e8e31970256079c398cf672427613a6c4
YellowNoise <morgankie@hotmail.com>
2018-03-31 05:08:25 -0700

    Added getQuote function

--- js/global.js
+++ js/global.js
@@ -3,3 +3,13 @@
 ** description: global javascript library containing code for use across the entire chrome extension, for
 **              unique functionality for specific pages, create and use a separate file named "pagename.js"
 */
+
+// get quote from encouragement.gunthercox.com
+function getQuote(){
+	return fetch("https://encouragement.gunthercox.com/api/encouragement/random/").then(function(response) {
+  		return response.json();	
+	}).then(function(data) {
+		return data.text
+	});
+}
+
