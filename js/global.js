5f445166bf67f53f318c19d37043a8aeb2f21d15
Victor Truong <me@ifvictr.com>
2018-03-31 13:23:15 +0000

    Use npm package for NLP

--- js/global.js
+++ js/global.js
@@ -3,63 +3,29 @@
 ** description: global javascript library containing code for use across the entire chrome extension, for
 **              unique functionality for specific pages, create and use a separate file named "pagename.js"
 */
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient();
 
 // get quote from encouragement.gunthercox.com, Returns Promise (string)
 function getQuote() {
-	return fetch("https://encouragement.gunthercox.com/api/encouragement/random/").then(function(response) {
-  		return response.json();	
-	}).then(function(data) {
-		return data.text
-	});
+	return fetch("https://encouragement.gunthercox.com/api/encouragement/random/")
+		.then(res => res.json())
+		.then(data => data.text);
 }
+
 //get Sentiment from Google Cloud API, Returns Promise (array of sentences)
 //each sentence has text and sentiment (magnitude and score)
-function getSentiment(textstr){
-	var data = {
-		encodingType: 'UTF8',
-  		document: {
-    		type: 'PLAIN_TEXT',
-    		content: textstr
-		} 
-	}	
-	return fetch("https://language.googleapis.com/v1/documents:analyzeSentiment", {
-    body: JSON.stringify(data), // must match 'Content-Type' header
-    credentials: 'same-origin', // include, same-origin, *omit
-    headers: {
-      'Authorization': 'insert key here',
-      'Content-Type': 'Content-Type: application/json; charset=utf-8'
-    },
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, cors, *same-origin
-    redirect: 'follow', // *manual, follow, error
-    referrer: 'no-referrer', // *client, no-referrer
-  }).then(function(response){
-  	return response.json().sentences
-  }); 
-}
-
 function getSentimentNode(text){
-	//import google cloud client lib
-	const language = require('@google-cloud/language');
- 
-	// Instantiates a client
-	const client = new language.LanguageServiceClient();
- 
- 
 	const document = {
-  	content: text,
-  	type: 'PLAIN_TEXT',
+	  	content: text,
+	  	type: 'PLAIN_TEXT',
 	};
- 
+	
 	// Detects the sentiment of the text
 	client
-	  .analyzeSentiment({document: document})
-	  .then(results => {
-	    return results[0].sentences;
-	  })
-	  .catch(err => {
-	    console.error('ERROR:', err);
-	  });
-
+		.analyzeSentiment({ document })
+		.then(results => results[0].sentences)
+		.catch(err => {
+			console.error('ERROR:', err);
+		});
 }
-
